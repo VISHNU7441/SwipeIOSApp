fddb19def158543b22567d611cd5b6f359fd9b80
VISHNU7441 <vishnurs74417441@gmail.com>
2025-02-07 02:08:17 +0530

    [feat]: update AddProductScreen  and AddProductScreenViewModel

--- SwipeIOSApp/Screens/AddProductScreen/View/AddProductScreen.swift
+++ SwipeIOSApp/Screens/AddProductScreen/View/AddProductScreen.swift
@@ -1,5 +1,5 @@
 //
-//  AddproductScreen.swift
+//  AddProductScreen.swift
 //  SwipeIOSApp
 //
 //  Created by vishnu r s on 03/02/25.
@@ -8,44 +8,110 @@
 import SwiftUI
 
 struct AddProductScreen: View {
+    @StateObject var viewModel = AddProductScreenViewModel()
     @Environment(\.dismiss) var dismiss
     @State var productName:String = ""
-    @State var price:Int?
-    @State var tax:Int?
+    @State var price:Int16?
+    @State var tax:Int16?
     @State var productType:String = ProductType.listOfProductTypes.first ?? ""
     @State var selectedImage: UIImage?
+    @State var inValid:CurrentInvalid = .OK
+    @FocusState var currentTextField:ProductFieldFocus?
     var body: some View {
-        VStack{
-            CustomTextField(title: "Product Name", symbol: nil) { TextField("", text: $productName) }
-            CustomTextField(title: "", symbol: "@") {
-                HStack{
-                    Picker("Product Type", selection: $productType) {
-                        ForEach(ProductType.listOfProductTypes, id: \.self){ productType in
-                            Text(productType)
-                                .tag(productType.description)
+        ScrollView{
+            VStack{
+                CustomTextField(title: "Product Name", symbol: nil, state: $inValid) {
+                    TextField("", text: $productName)
+                        .focused($currentTextField, equals: .productName)
+                        .onSubmit {
+                            currentTextField = .price
                         }
+                }
+                CustomTextField(title: "Product Type", symbol: "@", state: $inValid) {
+                    HStack{
+                        Picker("", selection: $productType) {
+                            ForEach(ProductType.listOfProductTypes, id: \.self){ productType in
+                                Text(productType)
+                                    .tag(productType.description)
+                            }
+                        }
+                        
+                        .pickerStyle(.navigationLink)
+                        Spacer()
                     }
-                    .pickerStyle(.navigationLink)
+                } // picker view
+                CustomTextField(title: "Price", symbol: "$", state: $inValid) {
+                    TextField("", value: $price, format: .number)
+                        .focused($currentTextField, equals: .price)
+                        .onSubmit {
+                            currentTextField = .tax
+                        }
+                }
+                CustomTextField(title: "Tax", symbol: "%", state: $inValid) {
+                    TextField("", value: $tax, format: .number)
+                        .focused($currentTextField, equals: .tax)
+                        .onSubmit {
+                          let _ = validateInput()
+                        }
+                }
+                HStack{
+                    CustomImagePicker(selectedImage: $selectedImage)
                     Spacer()
                 }
-            } // picker view
-            CustomTextField(title: "Price", symbol: "$") { TextField("", value: $price, format: .number)}
-            CustomTextField(title: "Tax", symbol: "%") { TextField("", value: $tax, format: .number) }
-            HStack{
-                CustomImagePicker(selectedImage: $selectedImage)
+            
+                Button("Upload Product") {
+                    validateAndUpload()
+                }
+                .buttonBorderShape(.capsule)
+                .buttonStyle(.borderedProminent)
+               
                 Spacer()
             }
-            Spacer()
-            Button("Upload Product") {
-                dismiss()
-            }
-            .buttonBorderShape(.capsule)
-            .buttonStyle(.borderedProminent)
-    
         }
         .padding()
         .navigationTitle("Add Product")
+        .toolbar{
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "house")
+                }
+
+            }
+        }
         .navigationBarBackButtonHidden()
+        .task {
+            await viewModel.upLoadProductsFromLocalStorage()
+        }
+    }
+    
+    func validateAndUpload(){
+        if validateInput(){
+            uploadData()
+        }
+    }
+    
+    func validateInput() -> Bool{
+        guard productName.count > 3 else { inValid = .productName ; return false}
+        guard !productType.isEmpty else { inValid = .productType ; return false}
+        guard price ?? 0 > 0 else { inValid = .price ; return false}
+        guard tax ?? 0 > 0 && tax ?? 0 < 50 else { inValid = .tax ; return false}
+        inValid = .OK
+        return true
+    }
+    
+    func uploadData(){
+        let newProduct = UploadProduct(
+            tax: tax ?? 0,
+            price: price ?? 0,
+            productType: productType,
+            productName: productName,
+            files: selectedImage
+        )
+        Task{
+            await viewModel.upLoadProduct(product: newProduct)
+        }
     }
     
 }
@@ -53,7 +119,8 @@ struct AddProductScreen: View {
 
 #Preview {
     NavigationStack{
-        SampleScreen()
+        AddProductScreen()
     }
 }
+
 
--- SwipeIOSApp/Screens/AddProductScreen/ViewModel/AddProductScreenViewModel.swift
+++ SwipeIOSApp/Screens/AddProductScreen/ViewModel/AddProductScreenViewModel.swift
@@ -6,26 +6,75 @@
 //
 
 import Foundation
+import Network
 
 class AddProductScreenViewModel:ObservableObject{
     
+    private let networkManager = NetworkManager.shared
+    private let coreDataManager = CoreDataManager.shared
+    private let monitor = NWPathMonitor()
+    @Published private var isOnline:Bool = true
+    var isAnyProductStoredLocally:Bool = false
+    var isThereAnyPendingProducts:Bool = true
     
-    let networkManager = NetworkManager.shared
+    init(){
+        print("addVm initialised")
+        setUpTheNetworkMonitor()
+    }
+    // Setup the network for monitor the internet connection
+    private func setUpTheNetworkMonitor(){
+         monitor.pathUpdateHandler = { path in
+             DispatchQueue.main.async {
+                 self.isOnline = path.status == .satisfied  // update the network status
+             }
+         }
+        let queue = DispatchQueue(label: "Network Monitor")
+        monitor.start(queue: queue)
+        print(isOnline.description)
+     }
+    
+    // Main function to upload product
+    func upLoadProduct(product:UploadProduct) async{
+        if isOnline{
+            await networkManager.uploadProduct(product)
+        }else{
+            print("no internet connection:\(isOnline.description)")
+            storeTheProductOffline(product: product)
+            self.isAnyProductStoredLocally = true
+        }
+    }
     
     
-//    func upLoadProduct(product:Product){
-//        networkManager.uploadData(product: product)
-//            .sink { completion in
-//                switch completion{
-//                case.finished:
-//                    print("successfully upload the data")
-//                case .failure(let error):
-//                    print("unable to upload the data due to error: \(error)")
-//                }
-//            } receiveValue: { product in
-//                print(product)
-//            }
-//            .store(in: &networkManager.cancellable)
-//
-//    }
+    func storeTheProductOffline(product:UploadProduct){
+        coreDataManager.saveProductToCoreData(product: product)
+    }
+    
+
+    func upLoadProductsFromLocalStorage() async{
+        if isOnline && (isAnyProductStoredLocally || isThereAnyPendingProducts){
+            let productsFromLocal = fetchTheProductsFromLocal()
+            
+            await withTaskGroup(of: Void.self) { group in
+                for product in productsFromLocal {
+                    group.addTask {
+                        await self.networkManager.uploadProduct(product)
+                        print("Product: \(product) from local uploaded successfully")
+                    }
+                }
+            }
+        }
+        isAnyProductStoredLocally = false
+        print("isAnyProductsStoredLocally:\(isAnyProductStoredLocally.description) && isThereAnyPendingProducts:\(isThereAnyPendingProducts.description)")
+    }
+    
+    private func fetchTheProductsFromLocal() -> [UploadProduct]{
+        let fetchedProducts = coreDataManager.fetchProductsFromCoreData() // a) will fetch the product
+        print(fetchedProducts.first?.productName as Any)
+        
+        coreDataManager.deleteProductsFromCoreData() // b) delete the product
+        
+        isThereAnyPendingProducts = coreDataManager.isThereAnyPendingProducts()
+        return fetchedProducts
+    }
+ 
 }
