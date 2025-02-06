87e6ced4714401e42a694696da98e87b131f1161
VISHNU7441 <vishnurs74417441@gmail.com>
2025-02-07 00:10:33 +0530

    [feat]: the search feature in the ListingProductScreen().

--- SwipeIOSApp/Screens/ListingProductScreen/View/SearchView/SearchProductView.swift
+++ SwipeIOSApp/Screens/ListingProductScreen/View/SearchView/SearchProductView.swift
@@ -8,11 +8,79 @@
 import SwiftUI
 
 struct SearchProductView: View {
+    @EnvironmentObject var viewModel:ListProductScreenViewModel
+    let product:Product
+    private var isAvailableInFavouriteList:Bool{
+        viewModel.isThisProductPresentInFavouriteList(product: product)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            ZStack{
+                AsyncImage(url: URL(string: product.image ?? "")){ result in
+                    switch result {
+                    case .empty:
+                        Image("default")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipped()
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipped()
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    case .failure:
+                        ProgressView()
+                    @unknown default:
+                        fatalError("Unable to process the image")
+                    }
+                    
+                }
+            }
+            .frame(width: 100, height: 100)
+            .padding(.horizontal)
+            
+            VStack(alignment:.leading, spacing: 7){
+                Text(product.productName.capitalized)
+                    .foregroundStyle(.primary)
+                    .bold()
+                    .lineLimit(1, reservesSpace: true)
+
+                Text(product.productType)
+                    .foregroundStyle(.secondary)
+                    .font(.subheadline)
+
+                HStack(spacing: 10){
+                    Text(String(format: "$%0.2f", product.price ?? 0))
+                        .bold()
+                        .font(.subheadline)
+                }
+            }
+            Spacer()
+            Image(systemName: "heart.fill")
+                .foregroundStyle(isAvailableInFavouriteList ? .swipeButtonColour : Color.white)
+                .imageScale(.large)
+                .padding(12)
+                .background{
+                    Circle()
+                        .fill(.gray.opacity(0.2))
+                }
+                .padding(.horizontal)
+        
+      }
+        .frame(height: 110)
+        .frame(maxWidth: .infinity)
+        .background{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+                .shadow(radius: 4)
+        }
+        
     }
 }
 
 #Preview {
-    SearchProductView()
+    SearchProductView(product: .sampleData)
+        .environmentObject(ListProductScreenViewModel())
 }
--- SwipeIOSApp/Screens/ListingProductScreen/View/SearchView/SearchResultView.swift
+++ SwipeIOSApp/Screens/ListingProductScreen/View/SearchView/SearchResultView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct SearchResultView: View {
+    @EnvironmentObject var viewModel:ListProductScreenViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment:.center){
+          if !viewModel.listOfSearchResults.isEmpty {
+                ScrollView{
+                    VStack{
+                        ForEach(viewModel.listOfSearchResults){ product in
+                            SearchProductView(product: product)
+                                .padding()
+                        }
+                    }
+                }
+          }else{
+              Text("Not found")
+          }
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(.ultraThinMaterial)
     }
 }
 
 #Preview {
     SearchResultView()
+        .environmentObject(ListProductScreenViewModel())
 }
