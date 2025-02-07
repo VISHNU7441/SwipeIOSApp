54607408d76a674478d88ef2d93c09afe999bb2d
VISHNU7441 <vishnurs74417441@gmail.com>
2025-02-07 23:03:36 +0530

    [feat]: add new upload success screen

--- SwipeIOSApp/Screens/AddProductScreen/View/SubViews/UploadSuccessScreen.swift
+++ SwipeIOSApp/Screens/AddProductScreen/View/SubViews/UploadSuccessScreen.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct UploadSuccessScreen: View {
+    @Binding var isOnline:Bool
+    @State private var isUploadSuccessful:Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30){
+            ZStack{
+                Circle()
+                    .foregroundStyle(isUploadSuccessful ? .blue.opacity(0.8) : .blue.opacity(0.2))
+                    .frame(width: isUploadSuccessful ? 300 : 0)
+                Image(systemName: "checkmark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: isUploadSuccessful ? 90 : 0)
+                    .foregroundStyle(isUploadSuccessful ? .white : .blue.opacity(0.2))
+            }
+            .frame(width: 350, height: 350)
+            
+            Text(isOnline ? "Upload Successful" : "Internet unavailable! \n product saved locally")
+                .lineLimit(2, reservesSpace: true)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(isUploadSuccessful ? .blue.opacity(0.9) : .blue.opacity(0.2))
+                .font(.system(size: isUploadSuccessful ? 25 : 0))
+               
+        }
+        .onAppear{
+            withAnimation(.easeIn(duration: 0.5)) {
+                isUploadSuccessful = true
+            }
+        }
     }
 }
 
 #Preview {
-    UploadSuccessScreen()
+    UploadSuccessScreen(isOnline: .constant(false) )
 }
